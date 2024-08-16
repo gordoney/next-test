6a0724a057dbbc92f709d00ec0666c11eaf83d13
Siarhei Balonikau <siarhei_balonikau@epam.com>
2024-08-16 11:30:53 +0400

    moonShaped

--- src/constants/graphql.ts
+++
@@ -1,2 +0,0 @@
-const CONTENTFUL_GRAPHQL_API = `https://graphql.contentful.com/content/v1/spaces/${import.meta.env.VITE_CONTENTFUL_SPACE_ID}/environments/${import.meta.env.VITE_CONTENTFUL_ENVIRONMENT}?access_token=${import.meta.env.VITE_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`;
-export default CONTENTFUL_GRAPHQL_API;
