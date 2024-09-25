bf15bd716506c976bb4816f15377668604589f9a
Siarhei Balonikau <siarhei_balonikau@epam.com>
2024-09-25 18:58:26 +0400

    test

--- src/components/organisms/Player/Player.tsx
+++ src/components/organisms/Player/Player.tsx
@@ -4,10 +4,6 @@ import "./Player.scss";
 import { useTranslations } from "next-intl";
 import styles from "./Player.module.scss";
 import PlayerButton from "../../atoms/PlayerButton/PlayerButton";
-import playIcon from "../../../assets/svg/play.svg";
-import pauseIcon from "../../../assets/svg/pause.svg";
-import forwardIcon from "../../../assets/svg/forward.svg";
-import rewindIcon from "../../../assets/svg/rewind.svg";
 
 interface Props {
   url: string;
@@ -20,17 +16,17 @@ export default function Player({ url }: Props) {
     <AudioPlayer
       customIcons={{
         play: PlayerButton({
-          iconUrl: playIcon,
+          iconUrl: "/svg/play.svg",
           iconAlt: t("playAudioguide"),
           text: t("playAudioguide"),
         }),
         pause: PlayerButton({
-          iconUrl: pauseIcon,
+          iconUrl: "/svg/pause.svg",
           iconAlt: t("pause"),
           text: t("playAudioguide"),
         }),
-        forward: <img className={styles.icon} src={forwardIcon} alt={t("forward")} />,
-        rewind: <img className={styles.icon} src={rewindIcon} alt={t("rewind")} />,
+        forward: <img className={styles.icon} src="/svg/forward.svg" alt={t("forward")} />,
+        rewind: <img className={styles.icon} src="/svg/rewind.svg" alt={t("rewind")} />,
       }}
       layout="stacked-reverse"
       customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
