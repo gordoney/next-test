57296a5b1db238bd4823b58a88e9d1b0bcd8ed7b
Siarhei Balonikau <siarhei_balonikau@epam.com>
2024-09-27 10:50:41 +0400

    1

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   const t = useTranslations();
 
   return (
-    <div className={styles.header} data-testid="header-component" data-test="test">
+    <div className={styles.header} data-testid="header-component" data-test="test1">
       <img className={styles.logo} src="/images/logo.png" alt={t("logo")} />
       <LanguageSwitcher />
     </div>
