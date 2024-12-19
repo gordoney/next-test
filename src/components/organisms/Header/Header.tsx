f175c98b318bf36eb5fe54c042537156c22b7ed1
Siarhei Balonikau <siarhei_balonikau@epam.com>
2024-12-19 18:42:24 +0400

    test3

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,13 +1,16 @@
+import Image from "next/image";
+
 import { useTranslations } from "next-intl";
 import styles from "./Header.module.scss";
 import LanguageSwitcher from "@/components/molecules/LanguageSwitcher/LanguageSwitcher";
+import logo from "@/assets/image/logo.png";
 
 export default function Header() {
   const t = useTranslations();
 
   return (
-    <div className={styles.header} data-testid="header-component" data-test="test5">
-      <img className={styles.logo} src="/images/logo.png" alt={t("logo")} />
+    <div className={styles.header} data-testid="header-component" data-test="test100">
+      <Image src={logo} width={68} alt={t("logo")} className={styles.logo} />
       <LanguageSwitcher />
     </div>
   );
